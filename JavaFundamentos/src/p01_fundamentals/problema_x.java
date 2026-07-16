2dfb23fb40cddc0fd46606ef7caae4d0a134fd14
Alejandro Valencia Mendoza <alejandro.valencia.mdza@gmail.com>
2026-07-16 11:56:05 -0600

    prueba de conexcion con repositorio

--- JavaFundamentos/src/p01_fundamentals/problema_x.java
+++ JavaFundamentos/src/p01_fundamentals/problema_x.java
@@ -3,7 +3,10 @@ package p01_fundamentals;
 
 public class problema_x {
     public static void main(String[] args) {
-        
+        /*
+        Esto es una prueba para ver si tengo conexion con git hub 
+        usando git
+        */
     }
     
 }
