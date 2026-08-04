632e1a0ed1f479904a4794f02ec4f631d3ba3380
Alejandro Valencia Mendoza <alejandro.valencia.mdza@gmail.com>
2026-08-04 01:18:59 -0600

    creacion de la clase pokemon sus atributos y 2 metodos

--- PokemonManager/src/Pokemon/Main.java
+++ PokemonManager/src/Pokemon/Main.java
@@ -1,10 +1,41 @@
 
 package Pokemon;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+      Pokemon pokemonUno = crearPokemon();
+      Pokemon pokemonDos = crearPokemon();
+      pokemonUno.atacar(pokemonDos);
+      pokemonDos.mostrarInformacion();
+
+    }
+    
+    public static Pokemon crearPokemon (){
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("Nombre del pokemon: ");
+        String nombre = sc.nextLine();
+        System.out.println("Ingresa el tipo: ");
+        String tipo= sc.nextLine();
+        System.out.println("ingresa el color: ");
+        String color = sc.nextLine();
+        System.out.println("Ingrese la estatura en metros: ");
+        double estatura =sc.nextDouble();
+        System.out.println("Ingrese el peso en kilogramos: ");
+        double peso= sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Ingrese su counter directo: ");
+        String counter = sc.nextLine();
+        System.out.println("Ingrese su evolucion: ");
+        String evolucion = sc.nextLine();
+        System.out.println(" ");
+        
+        Pokemon pokemonUno = new Pokemon (nombre, tipo,color,estatura, peso, counter, evolucion);
+        return pokemonUno;
+    
     }
     
 }
--- PokemonManager/src/Pokemon/Pokemon.java
+++ PokemonManager/src/Pokemon/Pokemon.java
@@ -7,14 +7,14 @@ public class Pokemon {
     private String color;/**/
     private double estatura;/**/
     private double peso;/**/
+    private String counter;/**/
+    private String evolucion;/**/
     
+    //valores que se inician en 1 o 0 
     private int nivel; 
     private int vida;
     private int ataque;
     private int defensa;
-    
-    private String counter;/**/
-    private String evolucion;/**/
 
     public Pokemon(String nombre, String tipo, String color, double estatura, double peso, String counter, String evolucion) {
         this.nombre = nombre;
@@ -116,6 +116,31 @@ public class Pokemon {
 
     public void setEvolucion(String evolucion) {
         this.evolucion = evolucion;
+    }
+    
+    public void entrenar (){
+        this.nivel++;
+        this.ataque+=5;
+        this.defensa+=3;
+        this.vida+=10;
+    }
+    public void mostrarInformacion(){
+        System.out.println("Nombre del pokemon: "+ nombre);
+        System.out.println("El tipo del pokemon es: "+tipo);
+        System.out.println("Color: "+color);
+        System.out.println("Estatura es: "+ estatura + " metros");
+        System.out.println("Peso: "+peso +" kg");
+        System.out.println("Nivel: "+nivel);
+        System.out.println("Vida: "+vida);
+        System.out.println("Counter: "+counter);
+        System.out.println("Evolucion: "+evolucion);
+    
+    }
+    public void atacar(Pokemon enemigo){
+        System.out.println("atacando a : "+enemigo.getNombre());
+        enemigo.setVida(enemigo.getVida() - ataque);
+    
+    
     }
     
 }
